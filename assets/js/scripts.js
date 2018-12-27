2fc985c85f7f88f8c79d394a9ecc1aecc6d88f6b
Jesus Francisco <jesusfrancisco@hotmail.es>
2018-12-27 16:49:50 -0500

    Se habilita el sw para que se registre en el navegador

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,14 +1,14 @@
 ((d,w,n,c)=>{
 
 if('serviceWorker' in navigator){
-	// w.addEventListener('load',()=>{
-	// 	n.serviceWorker.register('/espumredApp/sw.js').then((registration)=>{
-	// 		c(registration);
-	// 		c('Service Worker se ha registrado correctamente scope:',registration.scope);
-	// 	}).catch(err => {
-	// 		c('registro de service Worker fallido',err)
-	// 	});
-	// });
+	w.addEventListener('load',()=>{
+		n.serviceWorker.register('/espumredApp/sw.js').then((registration)=>{
+			c(registration);
+			c('Service Worker se ha registrado correctamente scope:',registration.scope);
+		}).catch(err => {
+			c('registro de service Worker fallido',err)
+		});
+	});
 }
 
 if(w.Notification && Notification.permission != 'denied'){
